8182653ee28cd5d93f9e3855822d48af383a8b6d
Prajeet Shrestha <pj.shrestha@andmine.com>
2015-03-19 15:38:10 +0545

    Added app delegate

---
+++ RadioButtons/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RadioButtons
+//
+//  Created by Prajeet Shrestha on 3/12/15.
+//  Copyright (c) 2015 Prajeet Shrestha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
